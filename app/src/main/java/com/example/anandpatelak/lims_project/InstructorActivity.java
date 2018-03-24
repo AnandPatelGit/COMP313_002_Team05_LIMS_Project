67d9c44cda3676dd50f941ddb4f7bb130b8e9ac3
thakkarraj99 <thakkarraj99@gmail.com>
2018-03-24 12:36:59 -0400

    Display Created folders to Instructors
    Instructors can see the folders created by them in instructor activity

--- app/src/main/java/com/example/anandpatelak/lims_project/InstructorActivity.java
+++ app/src/main/java/com/example/anandpatelak/lims_project/InstructorActivity.java
@@ -4,17 +4,66 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.R.layout;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class InstructorActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
     Button btnAddFolder, btnDisplayFolders;
-
+    ListView folderList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor);
         btnAddFolder = (Button) findViewById(R.id.addFolderButton);
+        folderList = (ListView) findViewById(R.id.listView);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("folders");
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.child("folderName").getValue(String.class);
+                arrayList.add(value);
+                arrayAdapter = new ArrayAdapter<String>(InstructorActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                folderList.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
     public void naviagateToNewFolderActivity(View view){
