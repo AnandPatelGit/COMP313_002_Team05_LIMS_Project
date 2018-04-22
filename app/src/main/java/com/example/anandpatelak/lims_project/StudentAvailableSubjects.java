4acbc7dd2f6cdb785e674e52a8e66ca915b56598
Anand Patel <patelanand9594@gmail.com>
2018-04-22 10:43:38 -0400

    Added Students Courses Support

--- app/src/main/java/com/example/anandpatelak/lims_project/StudentAvailableSubjects.java
+++ app/src/main/java/com/example/anandpatelak/lims_project/StudentAvailableSubjects.java
@@ -1,6 +1,5 @@
 package com.example.anandpatelak.lims_project;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,11 +10,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,52 +22,43 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentAvailableSubjects extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
     ArrayList<String> arrayList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
-    Button displayFeedback;
     ListView folderList;
-    String selectedSubjectStr;
+    TextView headerTextViewInstructor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
+        setContentView(R.layout.activity_instructor_available_subjects);
 
-        displayFeedback = (Button) findViewById(R.id.buttonSeeFeedback);
-        displayFeedback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayFeedback();
-            }
-        });
+        folderList = (ListView) findViewById(R.id.listVIewSubjects);
+
+        headerTextViewInstructor = (TextView)findViewById(R.id.textView16) ;
+
+
         Toolbar toolbar = findViewById(R.id.toolbar3);
         setSupportActionBar(toolbar);
-
-        Intent iin= getIntent();
-        Bundle b = iin.getExtras();
-
-        if(b!=null)
-        {
-            String j =(String) b.get("selected-subject");
-
-            selectedSubjectStr = j;
-        }
-
         firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("folders");
-        folderList = (ListView) findViewById(R.id.listViewFolders);
-        databaseReference.addChildEventListener(new ChildEventListener() {
+        databaseReference = firebaseDatabase.getReference("subjects");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
+        headerTextViewInstructor.setText("Available Course's for  Student "+email);
+
+        databaseReference.orderByChild("subjectProfessor").equalTo(email).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.child("folderName").getValue(String.class);
+                String value = dataSnapshot.child("subjectName").getValue(String.class);
                 arrayList.add(value);
-                arrayAdapter = new ArrayAdapter<String>(StudentActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                arrayAdapter = new ArrayAdapter<String>(StudentAvailableSubjects.this, android.R.layout.simple_list_item_1,arrayList);
                 folderList.setAdapter(arrayAdapter);
-                folderList.setOnItemClickListener(new ListClickHandler());
+                folderList.setOnItemClickListener(new StudentAvailableSubjects.FileListClickHandler());
             }
 
             @Override
@@ -91,33 +81,22 @@ public class StudentActivity extends AppCompatActivity {
 
             }
         });
-
-
-
     }
-
-    private void displayFeedback() {
-        Intent i = new Intent(this, DisplayFeedbackActivity.class);
-        startActivity(i);
-    }
-
-    public class ListClickHandler implements AdapterView.OnItemClickListener {
+    public class FileListClickHandler implements AdapterView.OnItemClickListener {
 
         @Override
         public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
             // TODO Auto-generated method stub
             //TextView listText = (TextView) view.findViewById(R.id.listText);
-            //TextView listText = (TextView) view.findViewById(R.id.listText);
             String text = ((TextView)view).getText().toString();
             // MyClass item = (MyClass) adapter.getItemAtPosition(position);
-            Intent intent = new Intent(StudentActivity.this, UploadFile.class);
-            intent.putExtra("selected-folder", text);
+            Intent intent = new Intent(StudentAvailableSubjects.this, StudentActivity.class);
+            intent.putExtra("selected-subject", text);
             startActivity(intent);
 
         }
 
     }
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.slider, menu);
